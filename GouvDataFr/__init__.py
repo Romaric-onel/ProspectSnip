ff2365b426e515178d141f5c43f38ae1475bfdcd
RomaricOnel <onelhounsinou@gmail.com>
2025-10-23 07:37:40 +0100

    feat:(api calling) finish asynchrone with 5 concurrents

--- GouvDataFr/__init__.py
+++ GouvDataFr/__init__.py
@@ -4,6 +4,8 @@ import openpyxl
 from decouple import config
 import asyncio
 from utils import HEADERS, clear_terminal, navigate_location, return_entry, verify_entry
-from urllib.parse import urlencode, urljoin
+from urllib.parse import urlencode
 import requests, json, pandas
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 API_DATA_GOUV_FR = config("ApiGouvEntreprise", cast=str, default=None)
--- GouvDataFr/use_api.py
+++ GouvDataFr/use_api.py
@@ -1,31 +1,42 @@
-from .create_parameters import  ask_scrap_information
-from . import urlencode, urljoin, API_DATA_GOUV_FR, json, pandas, requests
-from . import os, openpyxl, asyncio
+from . import API_DATA_GOUV_FR, HTTPAdapter, Retry, asyncio, json, requests, urlencode
+from .create_parameters import ask_scrap_information
+
+
 async def call_api_and_decode_result(info):
     query_string = urlencode(info)
     url = f"{API_DATA_GOUV_FR}search?{query_string}".replace("%2C", ",")
 
-    
-    try:
-        response = await asyncio.to_thread(requests.get, url, timeout=15)
-        response.raise_for_status()
-        print(response.content)
-        return json.loads(response.content.decode("utf-8"))
-    
-    except requests.exceptions.Timeout:
-        print("Votre machine présente un souci pour retourner la ressource\n.Réessayez puis contactez Hounsinou Romaric Onel si le problème persiste")
-    except requests.exceptions.ConnectionError:
-        print("Vous n'êtes pas connecté à Internet")
-    
-    except requests.exceptions.RequestException:
-        print("Un souci particulier se pose.\nEffectuez une capture d'écran, puis envoyez à Hounsinou Romaric Onel sur LinkedIn pour une assistance")
-    
-    
+    async with asyncio.Semaphore(5):
+        try:
+            response = await asyncio.to_thread(requests.get, url, timeout=15)
+            response.raise_for_status()
+            print(response.content)
+            return json.loads(response.content.decode("utf-8"))
+
+        except requests.exceptions.Timeout:
+            print(
+                "Votre machine présente un souci pour retourner la ressource\n.Réessayez puis contactez Hounsinou Romaric Onel si le problème persiste"
+            )
+        except requests.exceptions.ConnectionError:
+            print("Vous n'êtes pas connecté à Internet")
+
+        except requests.exceptions.RequestException:
+            print(
+                "Un souci particulier se pose.\nEffectuez une capture d'écran, puis envoyez à Hounsinou Romaric Onel sur LinkedIn pour une assistance"
+            )
+
     return None
+
 
 async def combine_all_result():
     info = ask_scrap_information()
-    
+    session = requests.Session()
+    retries = Retry(
+        total=3, backoff_factor=1, status_forcelist=(429, 500, 502, 503, 504)
+    )
+    session.mount("https://", HTTPAdapter(max_retries=retries))
+    session.mount("http://", HTTPAdapter(max_retries=retries))
+
     first_result = await call_api_and_decode_result(info)
     if not first_result:
         return []
